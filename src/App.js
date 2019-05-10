d6682d21b60af216fe663846c04f46d0d6a47e1a
petern160 <peternguyen160@gmail.com>
2019-05-09 17:18:18 -0700

    added css

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@
 import React, { Component } from "react";
 import Titles from "./components/Titles";
 import Form from "./components/Form";
-import Weather  from "./components/Weather"
+import Weather from "./components/Weather";
 
 // api key from apiweather
-const API_KEY = '050badc8a764c0398d3a0417c72737aa'
+const API_KEY = "050badc8a764c0398d3a0417c72737aa";
 
 class App extends Component {
   // can just do state no need for constructor and super
@@ -16,61 +16,73 @@ class App extends Component {
     country: undefined,
     humidity: undefined,
     description: undefined,
-    error: undefined,
+    error: undefined
   };
 
   // arrow function no need for binds
   // async await makes http calls easy
-  getWeather = async (e) => {
+  getWeather = async e => {
     // prevents page reload when you press get weather button
-    e.preventDefault()
+    e.preventDefault();
     // targets the form input with the correct name
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
-    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`)
+    const api_call = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
+    );
     // converts to json for fetch api
-    const data = await api_call.json()
+    const data = await api_call.json();
     // console.log(data)
 
     //  if city and country value added then render out the state
-    if(city && country) {
-    // updates state from undefined accordingly
-    this.setState ({
-      temperature: data.main.temp,
-      city: data.name,
-      country: data.sys.country,
-      humidity: data.main.humidity,
-      description: data.weather[0].description,
-      error : '',
-    })
-// error handles values
-  }else {
-    this.setState ({
-      temperature: undefined,
-      city: undefined,
-      country: undefined,
-      humidity: undefined,
-      description: undefined,
-      error: 'Please Enter Proper Values',
-    })
-  }
-}
-
+    if (city && country) {
+      // updates state from undefined accordingly
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+      // error handles values
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please Enter Proper Values"
+      });
+    }
+  };
 
   render() {
     return (
       <div>
-        <Titles />
-        {/* passes props down*/}
-        <Form getWeather ={this.getWeather}/>
-        <Weather 
-        temperature={this.state.temperature}
-        city={this.state.city}
-        country={this.state.country}
-        humidity={this.state.humidity}
-        description={this.state.description}
-        error={this.state.error}
-        />
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-xs-5 title-container">
+                  <Titles />
+                </div>
+                <div className="col-xs-7 form-container">
+                  <Form getWeather={this.getWeather} />
+                  <Weather
+                    temperature={this.state.temperature}
+                    city={this.state.city}
+                    country={this.state.country}
+                    humidity={this.state.humidity}
+                    description={this.state.description}
+                    error={this.state.error}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
